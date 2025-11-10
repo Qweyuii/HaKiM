f611236d8475cee82fa3d19334649f2471c081fb
Qweyuii <150279794+Qweyuii@users.noreply.github.com>
2025-11-10 21:33:38 +0100

    Change URL from key.txt to Approval.txt

--- E1A.py
+++ E1A.py
@@ -180,7 +180,7 @@ def superuser():
     uuid = str(os.geteuid()) + str(os.getlogin()) 
     id = "5".join(uuid)
     print(logo)
-    DARK=requests.get("https://github.com/Qweyuii/blob/main/key.txt").text
+    DARK=requests.get("https://github.com/Qweyuii/blob/main/Approval.txt").text
     if id in DARK:
         Main()
     else:
